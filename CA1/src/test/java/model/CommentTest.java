6a5727968abc79a74e4d4e2256d51312cd200810
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-10-21 11:40:59 +0330

    add test for comment methods

--- CA1/src/test/java/model/CommentTest.java
+++ CA1/src/test/java/model/CommentTest.java
@@ -1,4 +1,28 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CommentTest {
+
+    @Test
+    public void testGetCurrentDate() {
+        // Create a Comment object
+        Comment comment = new Comment(1, "user@example.com", "User", 123, "This is a test comment");
+
+        // Get the current date using the getCurrentDate method
+        String currentDate = comment.getCurrentDate();
+
+        // Create a SimpleDateFormat with the expected date format
+        SimpleDateFormat expectedDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // Format the current date with the expected format
+        String expectedDate = expectedDateFormat.format(new Date());
+
+        // Assert that the current date matches the expected date
+        assertEquals(expectedDate, currentDate);
+    }
 }
