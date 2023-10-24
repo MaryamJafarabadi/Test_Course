9f31b7eb8c6f2b0a7be92b153c46521b745e40e1
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-10-24 20:11:53 +0330

    edited user tests

--- CA1/src/test/java/model/CommodityTest.java
+++ CA1/src/test/java/model/CommodityTest.java
@@ -30,42 +30,50 @@ public class CommodityTest {
         assertEquals(PreInStock, commodity.getInStock());
     }
 
-
+    @Test
+    void testAddScoreSuccessfuly() {
+        assertDoesNotThrow(() -> commodity.addRate("User1", 4));
+        assertEquals(4, commodity.getUserRate().get("User1"));
+    }
     @Test
     void testAddRateValid() {
         assertDoesNotThrow(() -> commodity.addRate("User1", 4));
-        assertEquals(4, commodity.getUserRate().get("User1"));
         assertEquals(3.5f, commodity.getRating());
         // Initial rating was 3.0, so new rating is (3.0 + 4) / 2
     }
     @Test
     void testAddRateValid_line1() {
         assertDoesNotThrow(() -> commodity.addRate("User1", 1));
-        assertEquals(1, commodity.getUserRate().get("User1"));
         assertEquals((3+1)/2f, commodity.getRating());
     }
     @Test
     void testAddRateValid_line10() {
         assertDoesNotThrow(() -> commodity.addRate("User1", 10));
-        assertEquals(10, commodity.getUserRate().get("User1"));
         assertEquals((3+10)/2f, commodity.getRating());
     }
     @Test
     void testAddRateOutOfRange_LessThan1() {
         assertThrows(IllegalArgumentException.class, () -> commodity.addRate("user1", 0));
-        assertEquals(3f, commodity.getRating());
+        assertEquals(0.0f, commodity.getRating());
     }
 
     @Test
     void testAddRateOutOfRange_MoreThan10() {
         assertThrows(IllegalArgumentException.class, () -> commodity.addRate("user1", 11));
-        assertEquals(3f, commodity.getRating());
+        assertEquals(0.0f, commodity.getRating());
     }
 
     @Test
     void testAddRateOutOfRangeNegative() {
         assertThrows(IllegalArgumentException.class, () -> commodity.addRate("user1", -1));
-        assertEquals(3f, commodity.getRating());
+        assertEquals(0.0f, commodity.getRating());
+    }
+
+    @Test
+    void testAddRateOutOfRangeAfterAddingOneValidScore() { //because I want to rating be changed.
+        assertDoesNotThrow(() -> commodity.addRate("User1", 4));
+        assertThrows(IllegalArgumentException.class, () -> commodity.addRate("user1", -1));
+        assertEquals(3.5f, commodity.getRating());
     }
 
 }
