0dad6beb0b0efdc60ba197e655156422a6a33f1c
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-10-24 11:10:43 +0330

    test for commodity class
    but now the test for addRate failed

--- CA1/src/test/java/model/CommodityTest.java
+++ CA1/src/test/java/model/CommodityTest.java
@@ -1,4 +1,42 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import exceptions.NotInStock;
+
 public class CommodityTest {
+    private Commodity commodity;
+
+    @BeforeEach
+    void setUp() {
+        commodity = new Commodity();
+        commodity.setInStock(10);
+        commodity.setRating(3.0f);
+    }
+
+    @Test
+    void testUpdateInStockValid() {
+        assertDoesNotThrow(() -> commodity.updateInStock(5));
+        assertEquals(15, commodity.getInStock());
+    }
+
+    @Test
+    void testUpdateInStockInvalid() {
+        assertThrows(NotInStock.class, () -> commodity.updateInStock(-15));
+        assertEquals(10, commodity.getInStock());
+    }
+
+    @Test
+    void testRatingInitialization() {
+        assertEquals(3, commodity.getRating());
+    }
+
+    @Test
+    void testAddRate() {
+        commodity.addRate("User1", 4);
+        assertEquals(4, commodity.getUserRate().get("User1"));
+        assertEquals(3.5f, commodity.getRating()); // Initial rating was 3.0, so new rating is (3.0 + 4) / 2
+    }
 }
+
