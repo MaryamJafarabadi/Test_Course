b3bb05dfb73418be3c0d8905759a3491b383362e
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-10-24 10:12:42 +0330

    add test for addUserVote

--- CA1/src/test/java/model/CommentTest.java
+++ CA1/src/test/java/model/CommentTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CommentTest {
+    private Comment comment;
 
+    @BeforeEach
+    public void setUp() {
+        comment = new Comment(1, "user@example.com", "User1", 2, "This is a comment.");
+    }
     @Test
     public void testGetCurrentDate() {
         // Create a Comment object
@@ -25,4 +32,28 @@ public class CommentTest {
         // Assert that the current date matches the expected date
         assertEquals(expectedDate, currentDate);
     }
+
+
+    @Test
+    public void testAddUserVote() {
+        // Initial state of like and dislike should be 0
+        assertEquals(0, comment.getLike());
+        assertEquals(0, comment.getDislike());
+
+        // Add a "like" vote
+        comment.addUserVote("User2", "like");
+        assertEquals(1, comment.getLike()); // Like count should increase by 1
+        assertEquals(0, comment.getDislike()); // Dislike count should remain 0
+
+        // Add a "dislike" vote
+        comment.addUserVote("User3", "dislike");
+        assertEquals(1, comment.getLike()); // Like count should remain 1
+        assertEquals(1, comment.getDislike()); // Dislike count should increase by 1
+
+        // Add another "like" vote
+        comment.addUserVote("User4", "like");
+        assertEquals(2, comment.getLike()); // Like count should increase by 1
+        assertEquals(1, comment.getDislike()); // Dislike count should remain 1
+    }
+
 }
