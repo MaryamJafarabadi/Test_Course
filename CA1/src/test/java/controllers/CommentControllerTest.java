6c2ed2f6500903d20ffd78e6841f913ddc5a28c5
fatemeh 102 <88006802+F102M8@users.noreply.github.com>
2023-10-30 22:51:43 +0330

    add more code for CommentController that check value and body of response.

--- CA1/src/test/java/controllers/CommentControllerTest.java
+++ CA1/src/test/java/controllers/CommentControllerTest.java
@@ -29,7 +29,7 @@ public class CommentControllerTest {
 
     //likeComment
     @Test
-    public void likeCommentSuccess() throws Exception {
+    public void likeCommentSuccess_checkStatusOfResponse() throws Exception {
         Comment comment = mock(Comment.class);
         when(baloot.getCommentById(anyInt())).thenReturn(comment);
 
@@ -39,11 +39,37 @@ public class CommentControllerTest {
         ResponseEntity<String> response = commentController.likeComment("1", input);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    @Test
+    public void likeCommentSuccess_checkValueOfResponse() throws Exception {
+        Comment comment = mock(Comment.class);
+        when(baloot.getCommentById(anyInt())).thenReturn(comment);
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.likeComment("1", input);
+
+        assertEquals(200, response.getStatusCodeValue());
+
+    }
+    @Test
+    public void likeCommentSuccess_checkBodyOfResponse() throws Exception {
+        Comment comment = mock(Comment.class);
+        when(baloot.getCommentById(anyInt())).thenReturn(comment);
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.likeComment("1", input);
+
+        assertEquals("The comment was successfully liked!", response.getBody());
 
     }
 
+
     @Test
-    public void likeCommentNotExist() throws Exception {
+    public void likeCommentNotExist_checkStatusOfResponse() throws Exception {
         when(baloot.getCommentById(anyInt())).thenThrow(new exceptions.NotExistentComment());
 
         Map<String, String> input = new HashMap<>();
@@ -54,10 +80,33 @@ public class CommentControllerTest {
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
 
     }
+    @Test
+    public void likeCommentNotExist_checkValueOfResponse() throws Exception {
+        when(baloot.getCommentById(anyInt())).thenThrow(new exceptions.NotExistentComment());
 
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.likeComment("1", input);
+
+        assertEquals(404, response.getStatusCodeValue());
+
+    }
+    @Test
+    public void likeCommentNotExist_checkBodyOfResponse() throws Exception {
+        when(baloot.getCommentById(anyInt())).thenThrow(new exceptions.NotExistentComment());
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.likeComment("1", input);
+
+        assertEquals("Comment does not exist.", response.getBody());
+
+    }
     //dislikeComment
     @Test
-    public void dislikeCommentSuccess() throws Exception {
+    public void dislikeCommentSuccess_checkStatusOfResponse() throws Exception {
         Comment comment = mock(Comment.class);
         when(baloot.getCommentById(anyInt())).thenReturn(comment);
 
@@ -68,9 +117,32 @@ public class CommentControllerTest {
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
-
     @Test
-    public void dislikeCommentNotExist() throws Exception {
+    public void dislikeCommentSuccess_checkValueOfResponse() throws Exception {
+        Comment comment = mock(Comment.class);
+        when(baloot.getCommentById(anyInt())).thenReturn(comment);
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.dislikeComment("1", input);
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+    @Test
+    public void dislikeCommentSuccess_checkBodyOfResponse() throws Exception {
+        Comment comment = mock(Comment.class);
+        when(baloot.getCommentById(anyInt())).thenReturn(comment);
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.dislikeComment("1", input);
+
+        assertEquals("The comment was successfully disliked!", response.getBody());
+    }
+    @Test
+    public void dislikeCommentNotExist_checkStatusOfResponse() throws Exception {
         when(baloot.getCommentById(anyInt())).thenThrow(new exceptions.NotExistentComment());
 
         Map<String, String> input = new HashMap<>();
@@ -79,5 +151,27 @@ public class CommentControllerTest {
         ResponseEntity<String> response = commentController.dislikeComment("1", input);
 
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+    @Test
+    public void dislikeCommentNotExist_checkValueOfResponse() throws Exception {
+        when(baloot.getCommentById(anyInt())).thenThrow(new exceptions.NotExistentComment());
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.dislikeComment("1", input);
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+    @Test
+    public void dislikeCommentNotExist_checkBodyOfResponse() throws Exception {
+        when(baloot.getCommentById(anyInt())).thenThrow(new exceptions.NotExistentComment());
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "F102M8");
+
+        ResponseEntity<String> response = commentController.dislikeComment("1", input);
+
+        assertEquals("Comment does not exist.", response.getBody());
     }
 }
