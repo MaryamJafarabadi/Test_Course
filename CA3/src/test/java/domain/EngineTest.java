589bcd834cc43c026b1b1b5f369a750b226b8e7f
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-11-12 11:06:53 +0330

    cover testGetQuantityPatternByPrice

--- CA3/src/test/java/domain/EngineTest.java
+++ CA3/src/test/java/domain/EngineTest.java
@@ -42,5 +42,108 @@ public class EngineTest {
         assertEquals(3, engine.getAverageOrderQuantityByCustomer(1));
     }
 
+    @Test
+    public void testGetQuantityPatternByPriceWithNoOrderHistory() {
+        assertEquals(0, engine.getQuantityPatternByPrice(10));
+    }
+    @Test
+    public void testGetQuantityPatternByPriceWithEmptyHistory() {
+        Order order = new Order();
+        engine.orderHistory.add(order);
+        assertEquals(0, engine.getQuantityPatternByPrice(1));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPriceWithNoPatternAllDiffPrice() {
+        Order order = new Order();
+        order.setPrice(1);
+        order.setId(1);
+        Order order2 = new Order();
+        order2.setPrice(1);
+        order2.setId(2);
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        assertEquals(0, engine.getQuantityPatternByPrice(2));
+    }
+    @Test
+    public void testGetQuantityPatternByPriceDiffChange() {
+        Order order = new Order();
+        order.setPrice(1);
+        order.setId(1);
+        order.setQuantity(1);
+        Order order2 = new Order();
+        order2.setPrice(1);
+        order2.setId(2);
+        order2.setQuantity(2);
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        assertEquals(1, engine.getQuantityPatternByPrice(1));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPriceInterruptOrder() {
+        Order order = new Order();
+        order.setPrice(1);
+        order.setId(1);
+        order.setQuantity(1);
+        Order order2 = new Order();
+        order2.setPrice(1);
+        order2.setId(2);
+        order2.setQuantity(2);
+        Order order3 = new Order();
+        order3.setPrice(1);
+        order3.setId(3);
+        order3.setQuantity(4);
+
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        assertEquals(0, engine.getQuantityPatternByPrice(1));
+    }
+
+    @Test
+    public void testGetQuantityPatternByPrice() {
+        Order order = new Order();
+        order.setPrice(1);
+        order.setId(1);
+        order.setQuantity(1);
+        Order order2 = new Order();
+        order2.setPrice(1);
+        order2.setId(2);
+        order2.setQuantity(2);
+        Order order3 = new Order();
+        order3.setPrice(1);
+        order3.setId(3);
+        order3.setQuantity(3);
+
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        assertEquals(1, engine.getQuantityPatternByPrice(1));
+    }
+
+    /*@Test
+    public void testGetQuantityPatternByPriceNoPattern() {
+        Order order = new Order();
+        order.setPrice(1);
+        order.setId(1);
+        order.setQuantity(1);
+        Order order2 = new Order();
+        order2.setPrice(1);
+        order2.setId(2);
+        order2.setQuantity(2);
+        Order order3 = new Order();
+        order3.setPrice(1);
+        order3.setId(3);
+        order3.setQuantity(2);
+
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        assertEquals(0, engine.getQuantityPatternByPrice(1));
+    }*/
 
 }
