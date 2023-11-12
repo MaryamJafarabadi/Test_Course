77e0e1b7f1517c68555427d4b15f7b05955371d8
fatemeh 102 <88006802+F102M8@users.noreply.github.com>
2023-11-12 17:42:37 +0330

    add test for setter and getter for order class

--- CA3/src/test/java/domain/OrderTest.java
+++ CA3/src/test/java/domain/OrderTest.java
@@ -37,4 +37,35 @@ public class OrderTest {
         Order order = new Order();
         assertFalse(order.equals(new Object()));
     }
+    @Test
+    public void testSetGetId() {
+        Order order = new Order();
+        order.setId(1);
+        assertEquals(1, order.getId());
+    }
+
+
+    @Test
+    public void testSetCustomer() {
+        Order order = new Order();
+        order.setCustomer(1);
+        assertEquals(1, order.getCustomer());
+    }
+
+
+    @Test
+    public void testSetPrice() {
+        Order order = new Order();
+        order.setPrice(1);
+        assertEquals(1, order.getPrice());
+    }
+
+
+    @Test
+    public void testSetQuantity() {
+        Order order = new Order();
+        order.setQuantity(1);
+        assertEquals(1, order.getQuantity());
+    }
+
 }
