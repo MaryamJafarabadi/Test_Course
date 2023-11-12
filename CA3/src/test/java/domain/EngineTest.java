2a2308f2968798ebaafa84673d34e8c9891b09b6
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-11-12 10:00:22 +0330

    cover GetAverageOrderQuantityByCustomer

--- CA3/src/test/java/domain/EngineTest.java
+++ CA3/src/test/java/domain/EngineTest.java
@@ -1,11 +1,46 @@
 package domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class EngineTest {
+    private Engine engine;
+
+    @Before
+    public void setUp() {
+        engine = new Engine();
+    }
 
     @Test
-    public void justAnExample() {
-        // TODO
+    public void testGetAverageOrderQuantityByCustomerWithNoOrderHistory() {
+        assertEquals(0, engine.getAverageOrderQuantityByCustomer(1));
     }
+    @Test
+    public void testGetAverageOrderQuantityByCustomerSameCustomer() {
+        Order order = new Order();
+        Order order2 = new Order();
+        order.setCustomer(1);
+        order.setQuantity(3);
+        order2.setCustomer(1);
+        order2.setQuantity(2);
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        assertEquals(2, engine.getAverageOrderQuantityByCustomer(1));
+    }
+    @Test
+    public void testGetAverageOrderQuantityByCustomerDifferentCustomer() {
+        Order order = new Order();
+        Order order2 = new Order();
+        order.setCustomer(1);
+        order.setQuantity(3);
+        order2.setCustomer(2);
+        order2.setQuantity(2);
+        engine.orderHistory.add(order);
+        engine.orderHistory.add(order2);
+        assertEquals(3, engine.getAverageOrderQuantityByCustomer(1));
+    }
+
+
 }
